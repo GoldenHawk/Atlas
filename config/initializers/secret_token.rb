63a4c28704b88a3e8e554f6a8f4462c4d6719c5b
Jake Black <jakeenzo93@gmail.com>
2013-07-25 14:39:50 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlas::Application.config.secret_token = '6c12cfe4266170a92c2b9a60aaab790b86d343b5554c5a2f1c8f8cba41630c81c643f4c1eef22e00686cf230b686b29c3d21f8510eafbfe8c228bb385041d401'
